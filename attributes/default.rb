9e4e3de3cbba68a805f31a3534cbeede775ef137
Ryan Oberholzer <ryanoberholzer@gmail.com>
2013-11-24 16:00:44 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['rails-lastmile']['ruby_version'] = "1.9.3-p385"
 # when true, we reset the db using rake db:drop and rake db:setup
 default['rails-lastmile']['reset_db'] = false
 
-default['rails-lastmile']['environment'] = 'development'
+default['rails-lastmile']['environment'] = 'production'
 
 node.default[:unicorn][:worker_timeout] = 30
 node.default[:unicorn][:preload_app] = true
